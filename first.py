fe62046403007b2b3edfa3f661743fa3e7a63e90
AvinandanRoy <abhinandanroylipi@gmail.com>
2024-02-25 03:10:48 +0600

    nested for loop

---
+++ first.py
@@ -0,0 +1,20 @@
+
+# nested for loop
+print("Using Nested for Loop:")
+x =[ 1, 2]
+y =[3, 4 ]
+for i in x :
+    for j in y :
+        print(i,j)
+
+# we can use the while loop to replace the for loop :
+print("Using While loop:")
+a = [ 5,6]
+b  = [7,8]
+i = 0
+while i < len(a):
+    j= 0
+    while j < len(b):
+        print(a[i],b[j])
+        j += 1
+    i+=1
--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 
+
+
+print("I am Avinandan Roy.....")
+print("And this my first code of python that i pushed my git hub account ")
