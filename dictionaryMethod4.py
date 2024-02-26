f3342fb27f0f3c07e9ec35f883facb5b417065b3
AvinandanRoy <abhinandanroylipi@gmail.com>
2024-02-26 15:42:06 +0600

    dictionary practice 4

---
+++ dictionaryMethod4.py
@@ -0,0 +1,14 @@
+myInfo= {
+    'name' : 'Avinandan Roy',
+    'age' : 22 ,
+    "isHeGood" : True,
+    'isHeBad' : False,
+    'favouriteCode' : ['C','JAVA', 'JavaScript','Python']
+}
+
+myInfo.update({'fatherName' : 'Krishna Kanta Roy'})
+myInfo.update({'motherName' : 'Lipika Rani Roy'})
+print(myInfo)
+
+myInfo.update({'broName' : 'Pranta Roy','uniname':'DIU'})
+print(myInfo)
