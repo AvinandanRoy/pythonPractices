ff6318872ae02190087cab1a2dc88557317b3afb
AvinandanRoy <abhinandanroylipi@gmail.com>
2024-02-28 00:56:14 +0600

    Iteration done

---
+++ iteration1.py
@@ -0,0 +1,18 @@
+
+myName = 'Avinandan Roy'
+
+myit = iter(myName)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
